d23ee9a7648f45743fed247f2de60fb83334540c
RafaelGuardado <109107536+RafaelGuardado@users.noreply.github.com>
2023-01-26 15:45:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+ var canvas = new fabric.Canvas("myCanvas");
+// Create canvas variable
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
+{
+    // para enviar imagens
+	fabric.Image.fromURL(get_image, function(Img){
+	blockImageObject= Img;
+	blockImageObject.scaleToWidth(blockImageWidth);
+	blockImageObject.scaleToHeight(blockImageHeight);
+    blockImageObject.set({
+		top:blockY,
+		left:blockX
+	});
+    canvas.add(blockImageObject);
+    });
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '69') // adicione os códigos adequados às teclas
+	{
+		// enviar ranger escarlate
+		new_image('rr1.png');
+		console.log("r");
+	}
+	if(keyPressed == '86')
+	{
+		blockX = 200;
+		// enviar ranger verde
+		newImage('gr.png'); 
+		console.log("r");
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =350;
+		// enviar ranger amarelo
+		newImage('yr.png'); 
+		console.log("r");
+	}
+	if(keyPressed == '82')
+	{
+		blockX = 600;
+		// enviar ranger rosa
+		newImage('pr.png'); 
+		console.log("r");
+	}
+	if(keyPressed == '73')
+	{
+		blockX = 700;
+	   // enviar ranger índigo
+	   newImage('br.png'); 
+	   console.log("r");
+	}
+	
+}
+
